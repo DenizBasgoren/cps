73d758e43c188276156229583f28f0c3c1a9e17a
DenizBasgoren <denizbasgoren1997@gmail.com>
2017-09-20 01:12:33 +0300

    Add files via upload

---
+++ ez.js
@@ -0,0 +1,51 @@
+
+// MODEL
+var time = 0;
+var cps = Infinity;
+var record = Infinity;
+
+
+
+
+// CONTROLLER
+
+setInterval( function () {
+	time += 0.01;
+},10);
+
+window.onclick = function () {
+	cps = time;
+	time = 0;
+	if (cps < record) {
+		record = cps;
+	}
+	view();
+}
+
+
+
+
+// VIEW
+
+function view() {
+	document.getElementById('cps').innerHTML = cps.toFixed(2);
+	document.getElementById('record').innerHTML = record.toFixed(2);
+}
+
+setInterval(function () {
+	var korsan = document.getElementsByTagName('korsan')[0];
+	korsan.style.color = "#f00";
+
+	setTimeout(function () {
+		korsan.style.color = "#d00";
+	},200);
+
+},3000);
+
+
+// click
+//setinterval starts counting
+//click
+//interval is stopped
+//cps is calculated
+//time is emptied
